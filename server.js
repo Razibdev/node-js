34b4230bb41b8b125eb24e565aa7970ca5237812
Razib Hossen <razibhossendev@gmail.com>
2023-03-17 02:10:16 +0600

    razib

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ dotenv.config({
   path: "./config.env",
 });
 
-// const DB = process.env.DATABASE.replace(
-//   "<PASSWORD>",
-//   process.env.DATABASE_PASSWORD
-// );
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
 
 mongoose
-//   .connect(DB, {
-    .connect(process.env.DATABASE_LOCAL, {
+  .connect(DB, {
+//     .connect(process.env.DATABASE_LOCAL, {
     useNewUrlParser: true,
     // useCreateIndex: true,
     // useFindAndModify: false,
